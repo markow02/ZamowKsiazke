aaf542c9ac1744813401d331b93a6cb972958e39
markow02 <114979874+markow02@users.noreply.github.com>
2024-12-01 19:12:59 +0100

    Initial commit

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZamowKsiazke.Data;
+
+namespace ZamowKsiazke.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ZamowKsiazkeContext _context;
+        public StoreController(ZamowKsiazkeContext context) // Add constructor to initialize _context
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Book.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using ZamowKsiazke.Models;
+using Microsoft.EntityFrameworkCore;
+using ZamowKsiazke.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ZamowKsiazkeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ZamowKsiazkeContext")
+                         ?? throw new InvalidOperationException("Connection string 'ZamowKsiazkeContext' not found.")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Seedowanie danych
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        SeedData.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
