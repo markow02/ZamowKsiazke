78b0898813645faf5f2ec872d2282428d1058b81
M1kelos <128227021+M1kelos@users.noreply.github.com>
2024-12-10 11:45:15 +0100

    Update SeedData.cs

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -17,74 +17,197 @@ namespace ZamowKsiazke.Models
                 context.Book.AddRange(
                     new Book
                     {
-                        Title = "Bröderna Lejonhjärta",
-                        Language = "Swedish",
+                        Title = "Wiedźmin Rozdroże Kruków",
+                        Language = "Polski",
                         ISBN = "9789129688313",
-                        DatePublished = DateTime.Parse("2013-9-26"),
-                        Price = 139,
-                        Author = "Astrid Lindgren",
-                        Description = "A fantasy novel about two brothers who embark on a journey in the land of Nangijala.",
-                        ImageUrl = "/images/lejonhjärta.jpg"
+                        DatePublished = DateTime.Parse("2024-12-10"),
+                        Price = 49,
+                        Author = "Andrzej Sapkowski",
+                        Description = "Oto słowa Proroka: zaprawdę powiadam wam, że nastanie chaos w wielu miejscach i ogień często będzie wybuchał.
+                        Słońce rozjaśni się w nocy, a księżyc w ciągu dnia, gwiazdy zaś zaczną spadać. Krew będzie ciec z drzewa, a kamień wyda głos.
+                        Dzikie bestie będą atakować, a niewiasty brzemienne będą rodzić potwory.
+                        I oto sprawdziło się proroctwo i oczom naszym stało się ono widome: niewiasty porodziły potwory.
+                        Wiedźmini są tymi potworami, heroldami chaosu i zguby.
+                        Anonim, Monstrum albo wiedźmina opisanie",
+                        ImageUrl = "/images/rozdrozekrukow.jpg"
                     },
 
                     new Book
                     {
-                        Title = "The Fellowship of the Ring",
-                        Language = "English",
+                        Title = "Wiedźmin, Dzikie zwierzęta. Tom 8",
+                        Language = "Polski",
                         ISBN = "9780261102354",
-                        DatePublished = DateTime.Parse("1991-7-4"),
-                        Price = 100,
-                        Author = "J. R. R. Tolkien",
-                        Description = "The first part of J.R.R. Tolkien's epic adventure The Lord of the Rings.",
-                        ImageUrl = "/images/lotr.jpg"
+                        DatePublished = DateTime.Parse("2024-12-08"),
+                        Price = 39,
+                        Author = "Andrzej Sapkowski",
+                        Description = "Geralt wdaje się w sprzeczkę ze swoim zleceniodawcą, a gdy argumenty zostają zastąpione przez miecze i strzały, wiedźmin musi szukać ucieczki w morzu.",
+                        ImageUrl = "/images/dzikiezwierzeta.jpg"
                     },
 
                     new Book
                     {
-                        Title = "Mystic River",
-                        Language = "English",
+                        Title = "Wiedźmin Ziarno prawdy",
+                        Language = "Polski",
                         ISBN = "9780062068408",
-                        DatePublished = DateTime.Parse("2011-6-1"),
-                        Price = 91,
-                        Author = "Dennis Lehane",
-                        Description = "A crime thriller about three childhood friends whose lives are shattered by a murder.",
-                        ImageUrl = "/images/mystic-river.jpg"
+                        DatePublished = DateTime.Parse("2024-12-06"),
+                        Price = 29,
+                        Author = "Andrzej Sapkowski",
+                        Description = "Geralt w poszukiwaniu zlecenia trafia do tajemniczego starego dworu położonego w odludnej okolicy.",
+                        ImageUrl = "/images/ziarnoprawdy.jpg"
                     },
 
                     new Book
                     {
-                        Title = "Of Mice and Men",
-                        Language = "English",
+                        Title = "Im mroczniej tym lepiej",
+                        Language = "Polski",
                         ISBN = "9780062068408",
-                        DatePublished = DateTime.Parse("1994-1-2"),
-                        Price = 166,
-                        Author = "John Steinbeck",
-                        Description = "A novella about the dreams and struggles of two displaced migrant ranch workers.",
-                        ImageUrl = "/images/of-mice-and-men.jpg"
+                        DatePublished = DateTime.Parse("2024-10-2"),
+                        Price = 59,
+                        Author = "Stephen King",
+                        Description = "12 prawdziwych rarytasów od króla horroru. Od krótkich nowel, przez dłuższe opowiadania, po mini powieści.
+                        Od klasycznych horrorów, przez kryminały, po prozę obyczajową. Od Derry, przez Castle Rock, po Florydę
+                        Od dreszczy zgrozy, przez dramatyczne napięcie, po łzy rozbawienia.",
+                        ImageUrl = "/images/immroczniejtymlepiej.jpg"
                     },
 
                     new Book
                     {
-                        Title = "The Old Man and the Sea",
-                        Language = "English",
+                        Title = "Instytut",
+                        Language = "Polski",
                         ISBN = "9780062068408",
-                        DatePublished = DateTime.Parse("1994-8-18"),
-                        Price = 84,
-                        Author = "Ernest Hemingway",
-                        Description = "A short novel about an aging fisherman who struggles with a giant marlin far out in the Gulf Stream.",
-                        ImageUrl = "/images/old-man-and-the-sea.jpg"
+                        DatePublished = DateTime.Parse("2024-10-4"),
+                        Price = 59,
+                        Author = "Stephen King",
+                        Description = "Instytut Mocny jak To, przerażający jak Podpalaczka! Stephen King powraca do motywów znanych z jego najlepszych książek.",
+                        ImageUrl = "/images/instytut.jpg"
                     },
 
                     new Book
                     {
-                        Title = "The Road",
-                        Language = "English",
+                        Title = "Carrie",
+                        Language = "Polski",
                         ISBN = "9780307386458",
-                        DatePublished = DateTime.Parse("2007-5-1"),
-                        Price = 95,
-                        Author = "Cormac McCarthy",
-                        Description = "A post-apocalyptic novel about a father and son's journey through a devastated landscape.",
-                        ImageUrl = "/images/the-road.jpg"
+                        DatePublished = DateTime.Parse("2024-10-10"),
+                        Price = 29,
+                        Author = "Stephen King",
+                        Description = "Specjalne wydanie bestsellerowej powieści Carrie, która w 2024 r. obchodzi pięćdziesiątą rocznicę publikacji. W oprawie zintegrowanej, z barwionymi brzegami i przedmową autorstwa Margaret Atwood.",
+                        ImageUrl = "/images/carrie.jpg"
+                    },
+
+                    new Book
+                    {
+                        Title = "Szepty spoza nicości",
+                        Language = "Polski",
+                        ISBN = "9780062068987",
+                        DatePublished = DateTime.Parse("2022-10-4"),
+                        Price = 29,
+                        Author = "Remigiusz Mróz",
+                        Description = "Seweryn Zaorski po raz ostatni wraca do Żeromic, by sprzedać dom i na dobre zostawić za sobą tragiczną przeszłość.",
+                        ImageUrl = "/images/szepty.jpg"
+                    },
+                    
+                    new Book
+                    {
+                        Title = "Morderstwo w Mezopotamii",
+                        Language = "Polski",
+                        ISBN = "9780062068546",
+                        DatePublished = DateTime.Parse("2019-6-4"),
+                        Price = 27,
+                        Author = "Ahatha Christie",
+                        Description = "To kryminalna historia o tym jak jeden wakacyjny wyjazd może zamienić się w nieskończoną serię problemów.",
+                        ImageUrl = "/images/mezopotamia.jpg"
+                    },
+
+                    new Book
+                    {
+                        Title = "Venom II",
+                        Language = "Polski",
+                        ISBN = "9780062068123",
+                        DatePublished = DateTime.Parse("2021-2-2"),
+                        Price = 29,
+                        Author = "Aleksandra Kołaciuk",
+                        Description = "Drugi tom bestsellerowej serii! Historia lubi się powtarzać, Vivian przekonuje się o tym na własnej skórze. 
+                        Dziewczyna nie ma kontaktu z Venomem od czterdziestu trzech dni, w czasie których próbuje wrócić do normalnego życia i 
+                        zapomnieć o tym, co łączyło ją ze znanym przestępcą. Jednak doskonale zdaje sobie sprawę, że nadal potrzebuje jego pomocy.",
+                        ImageUrl = "/images/venom2.jpg"
+                    },
+
+                    new Book
+                    {
+                        Title = "Pacjentka",
+                        Language = "Polski",
+                        ISBN = "9780062068321",
+                        DatePublished = DateTime.Parse("2021-3-4"),
+                        Price = 39,
+                        Author = "Michaelides Alex",
+                        Description = "Czy człowiek może zmienić się w jednej chwili o 180 stopni? 
+                        Co takiego kryje się w ludzkiej psychice, że niektórzy są w stanie z zimną krwią pozbawiać życia innych? 
+                        Dowiedz się tego z jednego z najbardziej wyczekiwanych w 2019 roku thrillerów psychologicznych – „Pacjentka” autorstwa Alexa Michaelidesa.",
+                        ImageUrl = "/images/pacjentka.jpg"
+                    },
+
+                    new Book
+                    {
+                        Title = "Głusza",
+                        Language = "Polski",
+                        ISBN = "9780062068357",
+                        DatePublished = DateTime.Parse("2024-10-4"),
+                        Price = 49,
+                        Author = "Mark Edwards",
+                        Description = "Wypoczynek z dala od cywilizacji zmieni się w wakacyjny koszmar.",
+                        ImageUrl = "/images/glusza.jpg"
+                    },
+
+                    new Book
+                    {
+                        Title = "Cienie pośród mroku Tom 6",
+                        Language = "Polski",
+                        ISBN = "9780062068689",
+                        DatePublished = DateTime.Parse("2024-12-5"),
+                        Price = 59,
+                        Author = "Remigiusz Mróz",
+                        Description = "W życiu Seweryna i Burzy nie mogło wydarzyć się nic złego, nie kiedy wszystko wreszcie zaczęło im się układać. 
+                        Zamieszkali razem, zaczęli budować patchworkową rodzinę i uczyć się wspólnej egzystencji. 
+                        Spokój, który obydwoje odczuwali, zdawał się wieczny i niezachwiany. Nic nie zapowiadało nawałnicy, która miała na nich spaść.",
+                        ImageUrl = "/images/cienieposrodmroku.jpg"
+                    },
+
+                    new Book
+                    {
+                        Title = "Debiut",
+                        Language = "Polski",
+                        ISBN = "9780062068000",
+                        DatePublished = DateTime.Parse("2023-11-4"),
+                        Price = 25,
+                        Author = "Paula Świst",
+                        Description = "W relacjach Aresa i Niny zachodzi znacząca zmiana. 
+                        Do tej pory można je było określić jednym słowem: skomplikowane. Teraz stały się bardzo skomplikowane. A nawet bardziej niż bardzo.",
+                        ImageUrl = "/images/debiut.jpg"
+                    },
+
+                    new Book
+                    {
+                        Title = "Cienie",
+                        Language = "Polski",
+                        ISBN = "9780062068444",
+                        DatePublished = DateTime.Parse("2023-9-9"),
+                        Price = 55,
+                        Author = "Wojciech Chmielarz",
+                        Description = "Nowa powieść Wojciecha Chmielarza, laureata nagrody Wielkiego Kalibru.
+                        Komisarz Jakub Mortka powraca, by rozwikłać tajemnicze zabójstwo.",
+                        ImageUrl = "/images/cienie.jpg"
+                    },
+                    
+                    new Book
+                    {
+                        Title = "Opowiastki na zimowe wieczory. Kubuś i Przyjaciele",
+                        Language = "Polski",
+                        ISBN = "9780062068334",
+                        DatePublished = DateTime.Parse("2024-9-9"),
+                        Price = 35,
+                        Author = "Katarzyna Łączyńska",
+                        Description = "PRZYJAŹŃ, RADOŚĆ I MOC PRZYGÓD W STUMILOWYM LESIE.",
+                        ImageUrl = "/images/kubuspuchatek.jpg"
                     }
                 );
 
